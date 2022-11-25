1670321ea9625f56664cec3e6adab872af3ada5a
spring-hh <spring.hh.shu@gmail.com>
2022-11-25 22:50:55 +0900

    ログイン画面のモック

--- App.tsx
+++ App.tsx
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setName("");
+    setLoggedIn(false);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {loggedIn ? (
+        <View style={styles.container}>
+          <Text style={styles.title}>Welcome {name}!</Text>
+          <Button title="Logout" onPress={handleLogout} />
+        </View>
+      ) : (
+        <View style={styles.container}>
+          <TextInput
+            value={name}
+            onChangeText={(text) => setName(text)}
+            style={styles.inputfield}
+          />
+          <Button title="Login" onPress={() => handleLogin()} />
+          <StatusBar style="auto" />
+        </View>
+      )}
     </View>
   );
 }
@@ -13,8 +40,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    color: "red",
+  },
+  inputfield: {
+    borderColor: "red",
+    borderWidth: 1,
+    minWidth: 200,
   },
 });
